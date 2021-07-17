1a06678aca56ba3741550c0db3adfeb987c55314
Mohammad Hasani <the-dr-lazy@pm.me>
2021-07-17 22:09:40 +0430

    refactor(scripts/bootstrap): change idempontency error

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env sh
 
 DOTFILES_REPO_URL="https://github.com/the-dr-lazy/.dotfiles"
-DOTFILES="${HOME}/Projects/github/the-dr-lazy/.dotfiles"
+DOTFILES="$HOME/Projects/github/the-dr-lazy/.dotfiles"
 
 print_step() {
     GREEN='\033[0;32m'
     RESET='\033[0m' # No Color
-    printf "\n${GREEN}>>> ${1}${RESET}\n\n"
+    printf "\n$GREEN>>> $1$RESET\n\n"
 }
 
 if [ -d "$DOTFILES" ]; then 
-  printf "The system has been boostraped in the past."
+  printf ">>> The script is not idempotent. Trust me, you don't wanna run it for the second time!\n"
+  printf ">>> We're done."
   exit 1
 fi
 
@@ -34,10 +35,10 @@ print_step "Install nix-darwin."
 NIX_DARWIN_TEMP_DIR=$(mktemp --directory -t nix-darwin-XXXXX)
 nix-build https://github.com/LnL7/nix-darwin/archive/master.tar.gz -A installer -o "$NIX_DARWIN_TEMP_DIR/result"
 
-
 print_step "Clone .dotfiles repo."
-git clone "${DOTFILES_REPO_URL}" "${DOTFILES}"
-ln -isv "${DOTFILES}" "${HOME}/.config/nixpkgs"
+git clone "$DOTFILES_REPO_URL" "$DOTFILES"
+git submodule update --init
+ln -isv "$DOTFILES" "$HOME/.config/nixpkgs"
 
 print_step "Build the 1st home generation."
 home-manager switch
@@ -53,19 +54,28 @@ rm -rf "$DOTFILES/darwin-configuration.nix"
 print_step "Let's install Homebrew."
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)";
 
-print_step "Install apps from Homebrew."
-brew bundle --file "${DOTFILES}/config";
+print_step "Install Homebrew packages."
+brew bundle --file "$DOTFILES/config";
+
+#######################################################
+### DOOM Emacs
+
+print_step "Install DOOM Emacs."
+ln -sv "$DOTFILES/home/emacs.d" "$HOME/.emacs.d"
+$DOTFILES/home/emacs.d/bin/doom install
 
 #######################################################
 ### Keyboard
 
-sh "${DOTFILES}/home/keyboard/scripts/setup"
+print_step "Install keyboard."
+ln -sv "$DOTFILES/home/keyboard" "$HOME/.keyboard" 
+$DOTFILES/home/keyboard/scripts/setup
 
 #######################################################
 ### Mac OS X Configuration
 
 print_step "Configure Mac OS X."
-sh "${DOTFILES}/scripts/macos.sh";
+$DOTFILES/scripts/macos.sh
 
 #######################################################
 ### Done
